45c6b2e145d7be388c977caba91bf9b69bdf656e
volle1212 <94704890+volle1212@users.noreply.github.com>
2023-05-06 18:05:23 +0200

    bugfix

--- app/page.tsx
+++ app/page.tsx
@@ -16,79 +16,73 @@ function Home() {
 
     <div className="grid py-2 px-10">
 
-      <h1>Dryck</h1>
+      <h1 key="h0" >Dryck</h1>
 
-      <button className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 15); items.push("Kaffe/Te"); console.log(items)}}>
+      <button key="0" className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 15); items.push("Kaffe/Te"); console.log(items)}}>
         Kaffe/Te
       </button>
 
-      <button className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 10); items.push("Kaffe/Te (egen mugg)"); console.log(items)}}>
+      <button key="1" className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 10); items.push("Kaffe/Te (egen mugg)"); console.log(items)}}>
         Kaffe/Te (egen mugg)
       </button>
 
-      <button className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 20); items.push("Cola/Läsk/Loka"); console.log(items)}}>
+      <button key="2" className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 20); items.push("Cola/Läsk/Loka"); console.log(items)}}>
         Cola/Läsk/Loka
       </button>
 
-      <button className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 15); items.push("Festis"); console.log(items)}}>
+      <button key="3" className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 15); items.push("Festis"); console.log(items)}}>
         Festis
       </button>
 
       <h1>Smörgåsar</h1>
 
-      <button className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 20); items.push("Ost/Rökt skinka"); console.log(items)}}>
+      <button key="4" className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 20); items.push("Ost/Rökt skinka"); console.log(items)}}>
         Ost/Rökt skinka
       </button>
 
-      <button className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 30); items.push("Kötbullar"); console.log(items)}}>
+      <button key="5" className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 30); items.push("Kötbullar"); console.log(items)}}>
         Kötbullar rödbeta
       </button>
 
-      <button className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 30); items.push("Salami Brie"); console.log(items)}}>
+      <button key="6" className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 30); items.push("Salami Brie"); console.log(items)}}>
         Salamie Brie
       </button>
 
-      <h1>Kaffebröd</h1>
+      <h1 key="h1">Kaffebröd</h1>
 
-      <button className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 20); items.push("Hembakat"); console.log(items)}}>
+      <button key="7" className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 20); items.push("Hembakat"); console.log(items)}}>
         Hembakat
       </button>
 
-      <button className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 10); items.push("Småkaka"); console.log(items)}}>
+      <button key="8" className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 10); items.push("Småkaka"); console.log(items)}}>
         Småkaka
       </button>
 
-      <h1>Mat</h1>
+      <h1 key="h2">Mat</h1>
 
-      <button className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 50); items.push("Börgare/grillost"); console.log(items)}}>
+      <button key="9" className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 50); items.push("Börgare/grillost"); console.log(items)}}>
         Burgare/Grillost
       </button>
 
-      <button className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 20); items.push("Korv"); console.log(items)}}>
+      <button key="10" className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 20); items.push("Korv"); console.log(items)}}>
         Varmkorv
       </button>
 
-      <h1>Annat</h1>
+      <h1 key="h3">Annat</h1>
 
-      <button className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 10); items.push("Frukt"); console.log(items)}}>
+      <button key="11" className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 10); items.push("Frukt"); console.log(items)}}>
         Frukt
       </button>
 
-      <button className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 15); items.push("Choklad"); console.log(items)}}>
+      <button key="12" className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 15); items.push("Choklad"); console.log(items)}}>
         Kexchoklad, Chkladbitar
       </button>
 
-      <button className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 20); items.push("Godispåsar"); console.log(items)}}>
+      <button key="13" className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 20); items.push("Godispåsar"); console.log(items)}}>
         Godispåsar
       </button>
 
     </div>
-    
-    <div className='text-black'>{items.map( e =>
-          <div>{ e }</div>
-        )}
-        </div>
-
     </>
   );
 }
