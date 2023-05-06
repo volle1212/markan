986b41c94ffaf7c2665547e8c1067f4c7aa21d67
volle1212 <94704890+volle1212@users.noreply.github.com>
2023-05-06 17:58:34 +0200

    done

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,96 @@
-import Image from 'next/image'
+"use client"
+import React from 'react';
 
-export default function Home() {
+function Home() {
+  const [count, setCount] = React.useState(0);
+  var [items, setItems] = React.useState<string[]>([]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+    <header className="sticky top-0 z-50 flex justify-around">
+      <input value={count} className='text-black text-center text-2xl w-[40%]' />
+      <button className="bg-red-700 text-white text-2xl font-extrabold h-[100%] w-[30%]" onClick={() => { setCount(count - count); setItems(items=[]); console.log(items)}}>
+        CLEAR
+      </button>
+    </header>
+
+    <div className="grid py-2 px-10">
+
+      <h1>Dryck</h1>
+
+      <button className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 15); items.push("Kaffe/Te"); console.log(items)}}>
+        Kaffe/Te
+      </button>
+
+      <button className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 10); items.push("Kaffe/Te (egen mugg)"); console.log(items)}}>
+        Kaffe/Te (egen mugg)
+      </button>
+
+      <button className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 20); items.push("Cola/Läsk/Loka"); console.log(items)}}>
+        Cola/Läsk/Loka
+      </button>
+
+      <button className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 15); items.push("Festis"); console.log(items)}}>
+        Festis
+      </button>
+
+      <h1>Smörgåsar</h1>
+
+      <button className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 20); items.push("Ost/Rökt skinka"); console.log(items)}}>
+        Ost/Rökt skinka
+      </button>
+
+      <button className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 30); items.push("Kötbullar"); console.log(items)}}>
+        Kötbullar rödbeta
+      </button>
+
+      <button className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 30); items.push("Salami Brie"); console.log(items)}}>
+        Salamie Brie
+      </button>
+
+      <h1>Kaffebröd</h1>
+
+      <button className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 20); items.push("Hembakat"); console.log(items)}}>
+        Hembakat
+      </button>
+
+      <button className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 10); items.push("Småkaka"); console.log(items)}}>
+        Småkaka
+      </button>
+
+      <h1>Mat</h1>
+
+      <button className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 50); items.push("Börgare/grillost"); console.log(items)}}>
+        Burgare/Grillost
+      </button>
+
+      <button className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 20); items.push("Korv"); console.log(items)}}>
+        Varmkorv
+      </button>
+
+      <h1>Annat</h1>
+
+      <button className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 10); items.push("Frukt"); console.log(items)}}>
+        Frukt
+      </button>
+
+      <button className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 15); items.push("Choklad"); console.log(items)}}>
+        Kexchoklad, Chkladbitar
+      </button>
+
+      <button className="bg-gray-100 text-black p-7 my-2 active:bg-orange-500 focus:bg-orange-200" onClick={() => { setCount(count + 20); items.push("Godispåsar"); console.log(items)}}>
+        Godispåsar
+      </button>
+
+    </div>
+    
+    <div className='text-black'>{items.map( e =>
+          <div>{ e }</div>
+        )}
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>
+  );
 }
+
+export default Home;
